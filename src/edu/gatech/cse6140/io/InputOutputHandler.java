498bc74b613f2fb5c9fa45c74290d8ad25d8a30c
Nilaksh Das <nilakshdas@gatech.edu>
2016-11-08 01:37:10 -0500

    created I/O handler

---
+++ src/edu/gatech/cse6140/io/InputOutputHandler.java
@@ -0,0 +1,43 @@
+package edu.gatech.cse6140.io;
+
+import edu.gatech.cse6140.tsp.graph.Node;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class InputOutputHandler {
+    private String basePath;
+
+    public InputOutputHandler(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public ArrayList<Node> getNodesFromTSPFile(String fileName) {
+        ArrayList<Node> nodes = new ArrayList<>();
+
+        try {
+            InputStream in = new FileInputStream(new File(basePath + "/" + fileName));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            for (Integer i = 0; i < 5; i++) {
+                reader.readLine();
+            }
+
+            String line;
+
+            Integer n = 0;
+
+            while (!(line = reader.readLine()).equals("EOF")) {
+                String[] d = line.split(" ");
+
+                nodes.add(new Node(n++, d[0], Double.parseDouble(d[1]), Double.parseDouble(d[1])));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return nodes;
+    }
+}
