886e4d94c44654727c70ff1c25209e452e5beaac
Nilaksh Das <nilakshdas@gatech.edu>
2016-11-08 01:37:29 -0500

    added testing utility

---
+++ src/edu/gatech/cse6140/CodeTester.java
@@ -0,0 +1,23 @@
+package edu.gatech.cse6140;
+
+import edu.gatech.cse6140.io.InputOutputHandler;
+import edu.gatech.cse6140.tsp.graph.Node;
+
+import java.util.ArrayList;
+
+public class CodeTester {
+    public static void main(String[] args) {
+        InputOutputHandler ioHandler = new InputOutputHandler(
+                "/Users/nilakshdas/Grad School/Fall16/cse6140/Project/gatech-cse6140-fa16-z-tsp/data"
+        );
+
+        ArrayList<Node> nodes = ioHandler.getNodesFromTSPFile("Atlanta.tsp");
+
+        for (Node node1: nodes) {
+            for (Node node2: nodes) {
+                System.out.println(node1.getId()+" "+node2.getId());
+                System.out.println(node1.calculateDistanceFromNode(node2));
+            }
+        }
+    }
+}
