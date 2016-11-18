7f2fcbe8a1c3cf9e87c5efb5d7428c15d4155693
tsolanki95 <tsolanki95@yahoo.in>
2016-11-18 11:26:07 -0500

    unused imports

--- src/edu/gatech/cse6140/tsp/solvers/TravelingSalesmanProblemSolver.java
+++ src/edu/gatech/cse6140/tsp/solvers/TravelingSalesmanProblemSolver.java
@@ -1,9 +1,6 @@
 package edu.gatech.cse6140.tsp.solvers;
 
 
-import java.util.ArrayList;
-import edu.gatech.cse6140.graph.Node;
-
 import edu.gatech.cse6140.tsp.TravelingSalesmanTour;
 
 public interface TravelingSalesmanProblemSolver {
